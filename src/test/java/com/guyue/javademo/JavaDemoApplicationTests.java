7ef1622b8c9e7f61a79879e823ab5263398b712a
hupeiyong <hupeiyong@shuidi-inc.com>
2023-06-09 15:43:21 +0800

    Init

---
+++ src/test/java/com/guyue/javademo/JavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guyue.javademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
